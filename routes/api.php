ae18cfb2243e27ef519823b26b38e2417610915e
TamayGabriel <jgabriel.tamay@gmail.com>
2025-02-18 01:03:43 -0300

    agrego ruta de api de prueba db-test

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TorneoController;
 
 Route::post('crear-torneo', [TorneoController::class, 'crearTorneo']);
 Route::get('/torneos-finalizados', [TorneoController::class, 'obtenerTorneosFinalizados']);
+
+Route::get('/db-test', function () {
+    try {
+        // Intenta ejecutar una consulta simple para verificar la conexión
+        DB::select('SELECT 1');
+        return 'Conexión exitosa a PostgreSQL!';
+    } catch (\Exception $e) {
+        return 'Error de conexión: ' . $e->getMessage();
+    }
+});
